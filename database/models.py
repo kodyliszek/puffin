0b5b78f1da03c1352992cf22f90d23491cb78e19
damianfilipkowski <d.filipkowski@scopegroup.com>
2023-12-16 14:09:09 +0100

    feature/TICK-3 ConsumptionData and Report models added to models.py

---
+++ database/models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from .db import db
+
+
+class TimestampedDocument(db.Document):
+    meta = {"allow_inheritance": True, "abstract": True}
+
+    created_at = db.DateTimeField(required=True, default=datetime.now)
+    updated_at = db.DateTimeField(required=True, default=datetime.now)
+
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.now()
+        super().save(*args, **kwargs)
+
+
+class ConsumptionData(db.Document):
+    main_engine_consumption = db.FloatField()
+    aux_engine_consumption = db.FloatField()
+    aux_boiler_consumption = db.FloatField()
+    total_consumption = db.FloatField()
+
+
+class Report(TimestampedDocument):
+    report_number = db.IntField()
+    report_from = db.DateTimeField()
+    report_to = db.DateTimeField()
+    consumption = db.ReferenceField(ConsumptionData)
+
